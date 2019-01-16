66a363f6986e100a667f46eb05035e0d2f47d09e
ImaniiTy <augusto.vieira@dcomp.ufs.br>
2019-01-15 21:37:46 -0300

    testes

--- choi_yena/utils_/parameters.py
+++ choi_yena/utils_/parameters.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 viewDistance = 3
 reward_multiplier = 10
 maxHaliteToMove = 150
 maxHaliteToReturn = 850
 gamma = .9
-=======
-viewDistance = 0.0
-reward_multiplier = 5.0
-maxHaliteToMove = 902.0
-maxHaliteToReturn = 255.0
-gamma = 0.3
->>>>>>> ad8e7926d6f4028264d22d42773328c987d7973a
