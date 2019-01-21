6a74a702f0a3950024d43f23fba64612ea73e773
Anya51 <43707299+Anya51@users.noreply.github.com>
2019-01-21 02:31:27 -0300

    remove

--- Trabalho-IA-master/choi_yena/utils_/parameters_old.py
+++
@@ -1,5 +0,0 @@
-viewDistance = 4
-reward_multiplier = 10
-maxHaliteToMove = 150
-maxHaliteToReturn = 850
-gamma = .9
--- Trabalho-IA-master/setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='choi_yena', 
-      version='0.1',
-      packages=['choi_yena'])
